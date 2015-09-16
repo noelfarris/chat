e04ec92721d8a2ad203e641567bd311419b2a264
Noel Farris <noel.farris@gmail.com>
2015-09-16 16:50:41 -0500

    hard

--- scripts/main.js
+++ scripts/main.js
@@ -4,35 +4,47 @@ $(document).ready(function() {
 var message = $('#message');
 var submit = $('button');
 var messageList = $('#messageList')
+var userName = window.prompt('Enter your username');
+
+
 $('form').submit(function(e) {
 		e.preventDefault();
 		var newMessage = message.val();
 		$.post(
 			'http://tiyfe.herokuapp.com/collections/noel',
-			{chatInput: newMessage},
+			{chatInput: newMessage, UN: userName},
 			function(result) {
 				console.log(result._id);
-				messageList.append('<div>'+result.chatInput+'</div>');
+				messageList.append('<div>'+result.UN+ ": " + result.chatInput+'</div>');
 			},
 			'json'
 		);
 		message.val("");
 })
 
-function load() {$.get(
-		'http://tiyfe.herokuapp.com/collections/noel',
-		function(response) {
-			response.reverse();
-			for(var i=0; i<response.length; i++) {
-				messageList.append('<div>'+response[i].chatInput+'</div>');
-			}
-		},
-		'json'
-	);
-}
+// function load() {
+// 	$.get(
+// 		'http://tiyfe.herokuapp.com/collections/noel',
+// 		function(response) {
+// 			response.reverse();
+// 			for(var i=0; i<response.length; i++) {
+// 				messageList.append('<div>'+response[i].UN+ ": " +response[i].chatInput+'</div>');
+// 			}
+// 		},
+// 		'json'
+// 	);
+// }
 setInterval(function() {
-	messageList.html('');
-	load()
-}, 2000);
+$.get('http://tiyfe.herokuapp.com/collections/noel', function(data) {
+    messageList.html('');
+    data.reverse();
+    data.forEach(function(chat) {
+    	
+        messageList.append('<div>'+chat.UN+ ": " +chat.chatInput+'</div');
+    });    
+},
+     'json');
+
+}, 500);
 
 })
